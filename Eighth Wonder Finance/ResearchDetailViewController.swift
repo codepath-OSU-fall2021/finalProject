f510bafe1fc7ea80907621b067f73971f608b3d5
James Lipe <james@jameslipe.com>
2021-10-29 15:19:38 -0700

    add link and description to research detail view

--- Eighth Wonder Finance/ResearchDetailViewController.swift
+++ Eighth Wonder Finance/ResearchDetailViewController.swift
@@ -17,14 +17,26 @@ class ResearchDetailViewController: UIViewController {
             case url
         }
     }
+    
+    struct CompanyInfo : Codable {
+        var description: String
+        var website: String
+        
+        enum CodingKeys: String, CodingKey {
+            case description
+            case website
+        }
+    }
 
     var companyNameToDisplay = ""
     var companySymbol = ""
     var companyLogoURL = ""
     
     
+    @IBOutlet weak var websiteLink: UITextView!
     @IBOutlet weak var companyNameLabel: UILabel!
     @IBOutlet weak var companyLogoImage: UIImageView!
+    @IBOutlet weak var companyDescriptionView: UITextView!
     
     
     override func viewDidLoad() {
@@ -34,8 +46,48 @@ class ResearchDetailViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        
+        websiteLink.isEditable = false
+        websiteLink.dataDetectorTypes = UIDataDetectorTypes.all
         companyNameLabel.text = companyNameToDisplay
         getLogoUrl(symbol: companySymbol)
+        getCompanyInfo(symbol: companySymbol)
+    }
+    
+    func getCompanyInfo(symbol: String) {
+        let url = URL(string: "https://cloud.iexapis.com/stable/stock/\(symbol)/company?token=pk_246252e7872a41e4bb86d8c546d5e510")!
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: url) { data, response, error in
+            if error != nil {
+                print("Error: \(error)")
+                return
+            }
+            guard let httpResponse = response as? HTTPURLResponse,
+                  (200...299).contains(httpResponse.statusCode) else {
+                print("Bad http response code")
+                return
+            }
+            guard let mime = response?.mimeType, mime == "application/json" else {
+                print("Wrong MIME type!")
+                return
+            }
+            do {
+                let infoResponse = try JSONDecoder().decode(CompanyInfo.self, from: data!) as CompanyInfo
+                self.companyDescriptionView.text = infoResponse.description
+                self.websiteLink.text = infoResponse.website
+//                self.companyLogoURL = infoResponse.url
+//                let url = URL(string: self.companyLogoURL)
+//                if url == nil {
+//                    return
+//                } else {
+//                    self.companyLogoImage.af.setImage(withURL: url!)
+//                }
+            } catch {
+                print("JSON error: \(error.localizedDescription)")
+            }
+
+        }
+        task.resume()
     }
     
     func getLogoUrl(symbol: String) {

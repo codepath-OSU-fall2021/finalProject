666a57ef6aef87cecd097a36da1870544e6a71ef
James Lipe <james@jameslipe.com>
2021-11-09 14:09:13 -0800

    User stays logged in and alert shows for incorrect username/password

--- Eighth Wonder Finance/LoginViewController.swift
+++ Eighth Wonder Finance/LoginViewController.swift
@@ -24,6 +24,14 @@ class LoginViewController: UIViewController {
         
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let currentUser = PFUser.current()
+        if currentUser != nil {
+            self.performSegue(withIdentifier: "loignSegue", sender: nil)
+        }
+    }
+    
     @IBAction func onSignIn(_ sender: Any) {
         let username = usernameField.text!
         let password = passwordField.text!
@@ -33,6 +41,9 @@ class LoginViewController: UIViewController {
             if user != nil {
                 self.performSegue(withIdentifier: "loginSegue", sender: nil)
             } else {
+                let alert = UIAlertController(title: "Sign In Failed", message: error?.localizedDescription, preferredStyle: UIAlertController.Style.alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
                 print("Error: \(error?.localizedDescription)")
             }
         }
